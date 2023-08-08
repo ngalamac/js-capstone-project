d959ee856977bab64b8603546df5b2fe506f81df
wac <wababam9@gmail.com>
2023-08-08 04:24:16 -0700

    Add header html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 import './style.css';
-import image from './images/ic.png';
-
-function component(elt) {
-  const element = document.createElement(elt);
-  element.innerHTML = 'Hello Webpack';
-  return element;
-}
-
-const img = component('img');
-img.src = image;
-document.body.appendChild(img);
-document.body.appendChild(component('div'));
