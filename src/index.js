897b2237cfaf8bf4816f76bb3967a7e01cd45cab
wac <wababam9@gmail.com>
2023-08-09 05:44:23 -0700

    Add list nuber of likes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import DOMManipulator from './modules/dom.js';
 import './style.css';
+
+const dom = new DOMManipulator();
+dom.displayItems();
+const data = {
+  item_id: 'Under the dome',
+  username: 'Bish',
+  comment: 'This is a test comment',
+};
+dom.addComment(data);
