e8e92d5a5611c30d14e11072a8c8c3fc35342907
wac <wababam9@gmail.com>
2023-08-10 08:45:32 -0700

    Fix comment count update problem

--- src/test/counter.test.js
+++ src/test/counter.test.js
@@ -1,39 +1,39 @@
-import DOMManipulator from "../modules/dom.js";
+import DOMManipulator from '../modules/dom.js';
 
 const dom = new DOMManipulator();
 
-describe("Test show and comment counters", () => {
-  test("Test show counter", async () => {
+describe('Test show and comment counters', () => {
+  test('Test show counter', async () => {
     const shows = [
-      "Under the Dome",
-      "Person fo interest",
-      "Bitten",
-      "The Strain",
+      'Under the Dome',
+      'Person fo interest',
+      'Bitten',
+      'The Strain',
     ];
     expect(await dom.showCounter(shows)).toBe(4);
   });
 
-  test("Test comment counter", async () => {
-    const comment = jest.spyOn(dom, "getComment").mockImplementation(() => {
+  test('Test comment counter', async () => {
+    jest.spyOn(dom, 'getComment').mockImplementation(() => {
       const comments = [
         {
-          creation_date: "2023-03-12",
-          comment: "I love this show.",
-          username: "Wac",
+          creation_date: '2023-03-12',
+          comment: 'I love this show.',
+          username: 'Wac',
         },
         {
-          creation_date: "2023-03-12",
-          comment: "I love this show.",
-          username: "Wac",
+          creation_date: '2023-03-12',
+          comment: 'I love this show.',
+          username: 'Wac',
         },
         {
-          creation_date: "2023-03-12",
-          comment: "I love this show.",
-          username: "Wac",
+          creation_date: '2023-03-12',
+          comment: 'I love this show.',
+          username: 'Wac',
         },
       ];
       return comments.length;
     });
-    expect(await dom.commentCounter("Under the Dome")).toBe(3);
+    expect(await dom.commentCounter('Under the Dome')).toBe(3);
   });
 });
